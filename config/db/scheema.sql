723058c8f6cc865610bb7105c64cf2ab37b23c3b
jarora16 <71145914+jarora16@users.noreply.github.com>
2020-11-10 11:51:33 -0500

    base files for database

---
+++ config/db/scheema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS food_db;
+CREATE DATABASE food_db;
+USE food_db;
+
+CREATE TABLE generalOrder (
+    id int NOT NULL AUTO_INCREMENT,
+    customerName VARCHAR(255) NOT NULL,
+    numberOfBurgers INT NOT NULL,
+    itemsFromMenu VARCHAR(255) NOT NULL,
+    itemsFromSide VARCHAR (30) NOT NULL,
+    itemsFromDrinks VARCHAR (30) NOT NULL,
+    inProgress BOOLEAN NOT NULL DEFAULT false,
+    finishedORDER BOOLEAN NOT NULL DEFAULT false, 
+    totalOfCart INT NOT NULL,  
+    totalPrice DECIMAL(5,2) NOT NULL,
+    createdAt TIMESTAMP NOT NULL, 
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE burgers(
+    burgerID INT NOT NULL AUTO_INCREMENT,
+    typeOfBurger VARCHAR(255) NOT NULL, 
+    toppings VARCHAR(30) NOT NULL, 
+    PRIMARY KEY (burgerID)
+);
+
