93923e27fdfcda10155995af2d8ae9bc7d7b1fc9
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-11-12 11:09:13 -0500

    Added function to pull restaurant results

---
+++ public/js/yelp-fusion.js
@@ -0,0 +1,25 @@
+// Dependencies
+// ===========================================================
+const yelp = require("yelp-fusion");
+const restaurantResult = "Win+Son";
+const locationResult = "brooklyn,ny";
+const yelpApiKey = process.env.YELP_AK;
+
+const yelpSearchRequest = {
+    term: restaurantResult,
+    location: locationResult
+};
+
+const yelpClient = yelp.client(yelpApiKey);
+
+// Search Yelp
+// ===========================================================
+yelpClient.search(yelpSearchRequest).then(response => {
+    // eslint-disable-next-line prefer-destructuring
+    const result = response.jsonBody.businesses[0];
+    const resultJson = JSON.stringify(result, null, 4);
+    console.log(resultJson);
+})
+    .catch(err => {
+        console.log(err);
+    });
