39b2fa1ff603c1e1b9940dfd89b06945f1942c5e
Jay Arora <arorajay640@gmail.com>
2020-11-12 13:30:26 -0500

    fix errors

--- services/yelp-fusion.js
+++ services/yelp-fusion.js
@@ -8,8 +8,8 @@ const yelpApiKey = process.env.DB_YELP;
 
 
 const yelpSearchRequest = {
-    term: restaurantResult,
-    location: locationResult
+  term: restaurantResult,
+  location: locationResult
 };
 
 const yelpClient = yelp.client(yelpApiKey);
@@ -17,11 +17,11 @@ const yelpClient = yelp.client(yelpApiKey);
 // Search Yelp
 // ===========================================================
 yelpClient.search(yelpSearchRequest).then(response => {
-    // eslint-disable-next-line prefer-destructuring
-    const result = response.jsonBody.businesses[0];
-    const resultJson = JSON.stringify(result, null, 4);
-    console.log(resultJson);
+  // eslint-disable-next-line prefer-destructuring
+  const result = response.jsonBody.businesses[0];
+  const resultJson = JSON.stringify(result, null, 4);
+  console.log(resultJson);
 })
-    .catch(err => {
-        console.log(err);
-    });
+  .catch(err => {
+    console.log(err);
+  });
