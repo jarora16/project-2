c3a4be79e5e135ac7de40d5ee9f554eb1f79b5c1
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-11-12 21:30:58 -0500

    Updated API link to return specific restaurant data

--- public/js/places.js
+++ public/js/places.js
@@ -1,5 +1,5 @@
 function renderPlace() {
-  const placesUrl = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyAPG0uMATmuMmwuy_3nntET_8HdaykPkZE";
+  const placesUrl = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyAPG0uMATmuMmwuy_3nntET_8HdaykPkZE&place_id=ChIJ4-bYSvxbwokRPhLlLQFzOEo";
 
   $.ajax({
     url: placesUrl,
