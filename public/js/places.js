33d04a485b42de976c197773b7345a7a3ebc2b57
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-11-12 22:04:27 -0500

    Added const to hold each data we need to display

--- public/js/places.js
+++ public/js/places.js
@@ -6,6 +6,25 @@ function renderPlace() {
     method: "GET"
   }).then((response) => {
     console.log(response);
+    const places = response.result;
+    // Restaurant name
+    const resName = places.name;
+    // Restaurant rating
+    const resRating = places.rating;
+    // Total number of resturant reviews
+    const resNumRating = places.user_ratings_total;
+    // Restuarant price level
+    const resPrice = places.price_level;
+    // Is Resturant Open
+    const isOpen = places.opening_hours.open_now;
+    // Resturant hours
+    const resHours = places.opening_hours.weekday_text[4];
+    // Resturant website
+    const resWebsite = places.website;
+    // Resturant address
+    const resAddress = places.formatted_address;
+    // Resturant phone number
+    const resPhone = places.formatted_phone_number;
   });
 }
 
