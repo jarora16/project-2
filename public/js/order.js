71f3e94b880d739b5039572ed65592a6b51026a7
jarora16 <71145914+jarora16@users.noreply.github.com>
2020-11-13 14:07:48 -0500

    Update order.js

--- public/js/order.js
+++ public/js/order.js
@@ -5,7 +5,7 @@ $(document).ready(() => {
     console.log("Welcome brown bag! We look forward to serving you! Your table is ready now. Please proceed to table 12. Then when you are ready to eat please place your order on the app and our chefs will be happy to prepare it for you.Powered by Twilio.");
     testObject = {
       customerName: "Eric",
-      customerPhoneNumber: "5189514222",
+      customerPhoneNumber:
     };
   
     $.ajax({
