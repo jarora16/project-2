9a24eac2b968d6b430edd23dbdaf6378f3c3c652
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-11-12 22:33:30 -0500

    populate all information on page

--- public/js/places.js
+++ public/js/places.js
@@ -8,32 +8,32 @@ function renderPlace() {
     console.log(response);
     const places = response.result;
     // Restaurant name
-    const resName = $("<h2>").text(places.name);
-    $("#restaurant-info").append(resName);
+    const resName = $("<h1>").text(places.name);
+    $("#restaurant-name").append(resName);
     // Restaurant rating
-    const resRating = $("<p>").text(places.rating);
-    $("#restaurant-info").append(resRating);
+    const resRating = $("<h3>").text(places.rating + "/5");
+    $("#restaurant-rating").append(resRating);
     // Total number of resturant reviews
-    const resNumRating = $("<p>").text(places.user_ratings_total);
-    $("#restaurant-info").append(resNumRating);
+    const resNumRating = $("<p>").text(places.user_ratings_total + " Reviews");
+    $("#restaurant-numRating").append(resNumRating);
     // Restuarant price level
-    const resPrice = $("<p>").text(places.price_level);
-    $("#restaurant-info").append(resPrice);
+    const resPrice = $("<p>").text("Price: " + places.price_level + "/5");
+    $("#restaurant-price").append(resPrice);
     // Is Resturant Open
     const isOpen = $("<p>").text(places.opening_hours.open_now);
-    $("#restaurant-info").append(isOpen);
+    $("#restaurant-isopen").append(isOpen);
     // Resturant hours
     const resHours = $("<p>").text(places.opening_hours.weekday_text[4]);
-    $("#restaurant-info").append(resHours);
-    // Resturant website
-    const resWebsite = $("<p>").text(places.website);
-    $("#restaurant-info").append(resWebsite);
+    $("#restaurant-hours").append(resHours);
     // Resturant address
     const resAddress = $("<p>").text(places.formatted_address);
-    $("#restaurant-info").append(resAddress);
+    $("#restaurant-contact").append(resAddress);
     // Resturant phone number
     const resPhone = $("<p>").text(places.formatted_phone_number);
-    $("#restaurant-info").append(resPhone);
+    $("#restaurant-contact").append(resPhone);
+    // Resturant website
+    const resWebsite = $("<p>").text(places.website);
+    $("#restaurant-contact").append(resWebsite);
   });
 }
 
