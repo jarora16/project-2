8ea72aecd155e38f9ed69fe1907b3ee540c64062
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-11-12 22:07:13 -0500

    added HTML tags to each element

--- public/js/places.js
+++ public/js/places.js
@@ -8,31 +8,31 @@ function renderPlace() {
     console.log(response);
     const places = response.result;
     // Restaurant name
-    const resName = places.name;
+    const resName = $("<h2>").text(places.name);
     $("#restaurant-info").append(resName);
     // Restaurant rating
-    const resRating = places.rating;
+    const resRating = $("<p>").text(places.rating);
     $("#restaurant-info").append(resRating);
     // Total number of resturant reviews
-    const resNumRating = places.user_ratings_total;
+    const resNumRating = $("<p>").text(places.user_ratings_total);
     $("#restaurant-info").append(resNumRating);
     // Restuarant price level
-    const resPrice = places.price_level;
+    const resPrice = $("<p>").text(places.price_level);
     $("#restaurant-info").append(resPrice);
     // Is Resturant Open
-    const isOpen = places.opening_hours.open_now;
+    const isOpen = $("<p>").text(places.opening_hours.open_now);
     $("#restaurant-info").append(isOpen);
     // Resturant hours
-    const resHours = places.opening_hours.weekday_text[4];
+    const resHours = $("<p>").text(places.opening_hours.weekday_text[4]);
     $("#restaurant-info").append(resHours);
     // Resturant website
-    const resWebsite = places.website;
+    const resWebsite = $("<p>").text(places.website);
     $("#restaurant-info").append(resWebsite);
     // Resturant address
-    const resAddress = places.formatted_address;
+    const resAddress = $("<p>").text(places.formatted_address);
     $("#restaurant-info").append(resAddress);
     // Resturant phone number
-    const resPhone = places.formatted_phone_number;
+    const resPhone = $("<p>").text(places.formatted_phone_number);
     $("#restaurant-info").append(resPhone);
   });
 }
